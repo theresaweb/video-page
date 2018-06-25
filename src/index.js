028c5f0bd3f365d4de9e2925736490a7c985a2b8
Theresa Newman <theresa.webdev@gmail.com>
2018-06-25 19:44:19 -0400

    function not style

--- src/index.js
+++ src/index.js
@@ -8,7 +8,14 @@ class Video extends React.Component {
     var thisVideoIndex = this.props.videoIndex;
     return (
       <div>
-        <video controls src={this.props.playlist[thisVideoIndex].content_url} />
+        <video
+          id="activeVideo"
+          controls
+          src={this.props.playlist[thisVideoIndex].content_url}
+          onEnded={() => {
+            this.props.videoEnded();
+          }}
+        />
         <footer>{this.props.playlist[thisVideoIndex].title}</footer>
       </div>
     );
@@ -20,13 +27,38 @@ class VideoPlaylist extends React.Component {
     this.state = {
       error: "",
       playlist: data.playlist,
-      videoIndex: 0
+      videoIndex: 0,
+      activeIndex: 0,
+      videoCount: Object.keys(data.playlist).length
     };
   }
   handleThumbClick(index) {
     this.setState({
-      videoIndex: index
+      videoIndex: index,
+      activeIndex: index
     });
+  }
+  handleVideoEnd() {
+    var nextIndex = this.state.videoIndex + 1;
+    if (nextIndex < this.state.videoCount) {
+      this.setState({
+        videoIndex: nextIndex,
+        activeIndex: nextIndex
+      });
+      let vid = document.getElementById("activeVideo");
+      vid.onloadedmetadata = function() {
+        vid.play();
+      };
+    } else {
+      this.setState({
+        videoIndex: 0,
+        activeIndex: 0
+      });
+      let vid = document.getElementById("activeVideo");
+      vid.onloadedmetadata = function() {
+        vid.play();
+      };
+    }
   }
   render() {
     let thisPlaylist = this.state.playlist.slice();
@@ -35,14 +67,17 @@ class VideoPlaylist extends React.Component {
         <Video
           playlist={this.state.playlist}
           videoIndex={this.state.videoIndex}
+          videoEnded={this.handleVideoEnd.bind(this)}
         />
         <div className="videoCarousel">
           {thisPlaylist.map((video, index) => {
             var imgUrl = video.image_url + "?width=XXX";
+            const activeThumb =
+              this.state.activeIndex === index ? "activeThumb" : null;
+            const classes = `${activeThumb} videoThumb`;
             return (
-              <figure className="videoThumb">
+              <figure className={classes} key={index}>
                 <img
-                  key={index}
                   src={imgUrl}
                   alt={video.title}
                   onClick={() => this.handleThumbClick(index)}
